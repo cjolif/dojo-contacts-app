b8a8f78adfc44ad6bb698bac5cac278411e23723
Christophe Jolif <cjolif@gmail.com>
2013-04-10 22:41:23 +0200

    introduce FilteredListMixin

--- list.js
+++ list.js
@@ -1,4 +1,5 @@
-define(["dojo/_base/declare", "dojox/mobile/ListItem", "dojox/mobile/EdgeToEdgeStoreList"], function(declare, ListItem){
+define(["dojo/_base/declare", "dojox/mobile/ListItem", "dojox/mobile/EdgeToEdgeStoreList", "dojox/mobile/FilteredListMixin"],
+	function(declare, ListItem){
 	var ContactListItem = declare(ListItem, {
 		target: "detail",
 		clickable: true
---
+++ nls/list.js
@@ -0,0 +1,6 @@
+define({
+	root: {
+		search: "Search"
+	},
+	fr: true
+});
