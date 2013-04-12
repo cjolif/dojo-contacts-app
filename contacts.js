f3dcfa89808117f9049761897b7f95874734b674
Christophe Jolif <cjolif@gmail.com>
2013-04-12 13:10:40 +0200

    fix html5 history "detection"

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 require(["dojox/app/main", "dojo/sniff", "dojo/json", "dojo/text!./contacts.json", "dojox/mobile/common"],
 	function(Application, has, json, config){
 		// populate has flag on whether html5 history is correctly supported or not
-		has.add("html5history", has("ie") && (has("ie") <= 9));
+		has.add("html5history", !has("ie") || has("ie") > 9);
 		Application(json.parse(config));
 });
