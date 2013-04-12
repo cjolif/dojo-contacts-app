bdb188275352d235172dbd23bdb0a02fc301024c
Christophe Jolif <cjolif@gmail.com>
2013-04-12 16:14:54 +0200

    share  phone/tablet flag

--- contacts.js
+++ contacts.js
@@ -1,6 +1,7 @@
 require(["dojox/app/main", "dojo/sniff", "dojo/json", "dojo/text!./contacts.json", "dojox/mobile/common"],
-	function(Application, has, json, config){
+	function(Application, has, json, config, common){
 		// populate has flag on whether html5 history is correctly supported or not
 		has.add("html5history", !has("ie") || has("ie") > 9);
+		has.add("phone", ((window.innerWidth || document.documentElement.clientWidth) <= common.tabletSize));
 		Application(json.parse(config));
 });
--- detail.js
+++ detail.js
@@ -1,13 +1,12 @@
-define(["dojox/mobile/common", "dojox/mobile/Button", "dojox/mobile/FormLayout", "dojox/mobile/TextArea"],
-	function(common){
+define(["dojo/has", "dojox/mobile/Button", "dojox/mobile/FormLayout", "dojox/mobile/TextArea"],
+	function(has){
 	return {
 		beforeActivate: function(previousView){
+			// we set the back button label to the name of the previous view (taken from its nls)
 			this.backButton.set("label", previousView.nls.contacts);
-			if((window.innerWidth || document.documentElement.clientWidth) <= common.tabletSize){
-				this.backButton.domNode.style.display = "";
-			}else{
-				this.backButton.domNode.style.display = "none";
-			}
+			// we show/hide the back button based on whether we are on tablet or phone layout, as we have two panes
+			// in tablet it makes no sense to get a back button
+			this.backButton.domNode.style.display = has("phone")?"":"none";
 		}
 	}
 });
