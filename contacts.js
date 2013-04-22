25db09d5f05bbee5812d243764063382c10b201c
Christophe Jolif <cjolif@gmail.com>
2013-04-22 18:14:39 +0200

    make sure the file can be loaded from somewhere else

--- contacts.js
+++ contacts.js
@@ -1,9 +1,9 @@
 // we use define and not require to workaround Dojo build system limitation that prevents from making of this file
 // a layer if it using require as it should be not define
-define(["dojox/app/main", "dojo/sniff", "dojo/json", "dojo/text!./contacts.json", "dojox/mobile/common"],
+define(["dojox/app/main", "dojo/sniff", "dojo/json", "dojo/text!contactsApp/contacts.json", "dojox/mobile/common"],
 	function(Application, has, json, config, common){
 		// populate has flag on whether html5 history is correctly supported or not
 		has.add("html5history", !has("ie") || has("ie") > 9);
 		has.add("phone", ((window.innerWidth || document.documentElement.clientWidth) <= common.tabletSize));
 		Application(json.parse(config));
-});
+	});
